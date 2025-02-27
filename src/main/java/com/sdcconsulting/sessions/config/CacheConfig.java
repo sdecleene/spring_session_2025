4714b8c5e3028c53fc2c7ed08d7e1c46d5258fa3
sdecleene <decleene.steven@gmail.com>
2025-02-27 23:28:23 +0100

    10 - Caching

--- src/main/java/com/sdcconsulting/sessions/config/CacheConfig.java
+++ src/main/java/com/sdcconsulting/sessions/config/CacheConfig.java
@@ -1,14 +1,20 @@
 package com.sdcconsulting.sessions.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.List;
 
+@Slf4j
 @Configuration
 @EnableCaching
+@EnableScheduling
 public class CacheConfig implements CacheManagerCustomizer<ConcurrentMapCacheManager> {
 
     public static final String CACHE_SISA_IDS = "sisaIds";
@@ -18,6 +24,12 @@ public class CacheConfig implements CacheManagerCustomizer<ConcurrentMapCacheMan
         cacheManager.setCacheNames(List.of(
                 CACHE_SISA_IDS
         ));
+    }
+
+    @CacheEvict(value = CACHE_SISA_IDS, allEntries = true)
+    @Scheduled(fixedRateString = "${spring.cache.sisaIds}")
+    public void emptySisaIdsCache() {
+        log.info("Emptying the sisaIds Cache");
     }
 
 }
