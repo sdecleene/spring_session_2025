c2cacb34b0a7ed6ff08249b684eaca8c313161b2
sdecleene <decleene.steven@gmail.com>
2025-02-27 22:07:11 +0100

    06 - Profiles

--- src/main/java/com/sdcconsulting/sessions/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sdcconsulting/sessions/controller/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.sdcconsulting.sessions.service.exception.StudentNotFoundException;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
+@Profile("!DEV")
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
